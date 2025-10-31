c7c014125e629f0b8ea2859ecc4a722af4f08ed2
Edward Z. Yang <ezyang@meta.com>
2025-10-31 12:33:42 -0700

    Make PT2 compile backprop through custom op without autograd key a hard error
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    ghstack-source-id: 71bae380461108f542c24d15dc03ad1b6abbab00
    Pull-Request: https://github.com/pytorch/pytorch/pull/166367

--- torch/_higher_order_ops/effects.py
+++ torch/_higher_order_ops/effects.py
@@ -26,7 +26,10 @@ OpType = Union[torch._ops.HigherOrderOperator, torch._ops.OpOverload]
 
 SIDE_EFFECTS = WeakKeyDictionary[OpType, _EffectType](
     [
+        (torch.ops.aten._print, _EffectType.ORDERED),
         (torch.ops.aten._print.default, _EffectType.ORDERED),
+        (torch.ops.aten._async_error, _EffectType.ORDERED),
+        (torch.ops.aten._async_error.default, _EffectType.ORDERED),
         (call_torchbind, _EffectType.ORDERED),
     ]
 )
